6c7323fc60c706c9f4c94e525d4f0e6b7be74698
Duane Carver <duane.carver.aw@gmail.com>
2012-08-25 14:43:57 -0400

    Adding initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AW::Application.config.secret_token = '5ec76efae33b589d7c99f5318713431b4e3cddb0d47483dfad8f13fbe775dc8febd80d30ec0c7b5b134428b1f8e738e8ac28a21efbbf485c1631c9b2120df4df'
